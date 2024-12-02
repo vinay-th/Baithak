47548c19e23138875b515d22a061eb3f7e659834
Vinay Thakor <vinay.th.24@gmail.com>
2024-12-02 22:23:20 +0530

    remove unused backend files and configurations, add initial TypeScript setup

---
+++ back/prisma/migrations/20241202165146_users_table/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "image" VARCHAR(255),
+    "oauth_id" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
